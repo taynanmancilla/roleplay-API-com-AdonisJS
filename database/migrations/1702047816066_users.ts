f3cf44819f91f44f3febb7afa4fa407450926c07
Taynan <taynan.estudo@gmail.com>
2023-12-08 13:15:30 -0400

    introMigrations_addUserMigration

--- database/migrations/1702047816066_users.ts
+++ database/migrations/1702047816066_users.ts
@@ -3,18 +3,19 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'users'
 
+  // Cria tabela
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.string('email').notNullable().unique()
+      table.string('username').notNullable().unique()
+      table.string('password').notNullable()
+      table.string('avatar').defaultTo('') // Pra ajudar o front a lidar com usuario que nao colocam avatar
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
   }
-
+  // Remove tabela
   public async down () {
     this.schema.dropTable(this.tableName)
   }
