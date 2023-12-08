fb5d872e2e8b5f12122d58837a907b38084a7d81
Taynan <taynan.estudo@gmail.com>
2023-12-08 09:25:08 -0400

    introControler_addUserRoute

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,8 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+// Route.post('/users', async ({ response }) => {
+//   return response.created({})
+// })
+Route.post('/users', 'UsersController.store')
---
+++ test/users/users.spec.ts
@@ -0,0 +1,35 @@
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+/*
+  {
+    "users": {
+      "id": number,
+      "email": string,
+      "username": string,
+      "password": string,
+      "avatar": string
+    }
+  }
+*/
+test.group('User', () => {
+  // Usando o 'Only' somente esse teste executa
+  test.only('Deve criar um usuario', async (assert) => {
+    // Criando o corpo da requisicao(conjunto de informacoes que pertencem ao usuario)
+    const userPayload = {
+      email: 'test@test.com',
+      username: 'test',
+      password: 'test',
+      avatar: 'http://urldaimagem',
+    }
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    assert.exists(body.user, 'User Undefined') // Se nao retornar usuario da esse erro
+    assert.exists(body.user.id, 'ID Undefined') // Se nao retornar ID da esse erro
+    assert.equal(body.user.email, userPayload.email) // Compara o email com o email do payload
+    assert.equal(body.user.username, userPayload.username) // Compara o username com o username do payload
+    assert.equal(body.user.password, userPayload.password) // Compara o password com o password do payload
+    assert.equal(body.user.avatar, userPayload.avatar) // Compara o avatar com o avatar do payload
+  })
+}) // Agrupar varios testes
