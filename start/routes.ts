f30b1f6555600ea923160801888f88c0635a9243
Taynan <taynan.estudo@gmail.com>
2023-12-15 10:38:14 -0400

    test_forgotPassword

--- start/routes.ts
+++ start/routes.ts
@@ -29,3 +29,4 @@ Route.get('/', async () => {
 // })
 Route.post('/users', 'UsersController.store')
 Route.put('/users/:id', 'UsersController.update')
+Route.post('/forgot-password', 'PasswordsController.forgotPassword')
---
+++ test/users/passwords.spec.ts
@@ -0,0 +1,27 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { UserFactory } from 'Database/factories'
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Password', (group) => {
+  test.only('Deve enviar um email com instrucoes de redefinicao de senha', async (assert) => {
+    const user = await UserFactory.create()
+
+    await supertest(BASE_URL)
+      .post('/forgot-password')
+      .send({
+        email: user.email,
+        resetPassword: 'url',
+      })
+      .expect(204)
+  })
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+})
